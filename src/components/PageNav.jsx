5d610443c9ed48f38d28ea6f35fbfecbcaee5a21
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-05-04 22:48:45 +0300

    using css modules

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,18 +1,21 @@
 import { NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
 
 function PageNav() {
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      <li>
-        <NavLink to="/pricing">Pricing</NavLink>
-      </li>
-      <li>
-        <NavLink to="/product">Product</NavLink>
-      </li>
-    </ul>
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
