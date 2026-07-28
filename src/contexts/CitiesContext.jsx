aa7a053bb4f7903cd62bbe4be505f7b7190553d0
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-07-28 11:31:12 +0300

    consuming the cities context

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,10 +1,10 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const BASE_URL = "http://localhost:9000";
 const CitiesContext = createContext();
 
 function CitiesProvider({ children }) {
-  const [cities, setCiteis] = useState([]);
+  const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
@@ -13,7 +13,7 @@ function CitiesProvider({ children }) {
         setIsLoading(true);
         const res = await fetch(`${BASE_URL}/cities`);
         const data = await res.json();
-        setCiteis(data);
+        setCit(data);
       } catch {
         alert("There was an error loading data...");
       } finally {
@@ -23,7 +23,18 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  return <CitiesContext.Provider value={cities, isLoading}>{children}</CitiesContext.Provider>;
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext used outside the CitiesProvider");
+  return context;
+}
+
+export { CitiesProvider, useCities };
