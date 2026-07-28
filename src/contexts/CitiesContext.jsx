803a110814a54613aed0220c855175913ba1a710
Ephrem216 <156383015+Ephrem216@users.noreply.github.com>
2026-07-28 09:55:16 +0300

    creating a CitiesContext

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:9000";
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCiteis] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCiteis(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return <CitiesContext.Provider value={cities, isLoading}>{children}</CitiesContext.Provider>;
+}
+
+export { CitiesProvider };
